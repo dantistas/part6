8a32d373e2a1a79e2774b342f2297294a8d015d9
dantistas <rytisvenslovas@gmail.com>
2020-07-16 20:35:08 +0200

    removed couple lines from filter component which i forgot to remove previously

--- redux-anecdotes/src/components/Filter.js
+++ redux-anecdotes/src/components/Filter.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+
 
 const Filter = ({dispatch, filter}) => {
-  const filterState = useSelector(state => state.filter)
   const handleChange = (event) => {
       dispatch(filter(event.target.value))
   }
